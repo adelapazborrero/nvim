b36a2626549328a69e52086fc8bc08da83ec2dba
Abel de la Paz <abel45991690@gmail.com>
2023-03-14 15:07:37 +0100

    chore: cleanup

--- lua/setup/nvim-tree.lua
+++ lua/setup/nvim-tree.lua
@@ -7,8 +7,7 @@ end
 tree.setup({
 	disable_netrw = true,
 	hijack_netrw = true,
-	-- open_on_setup = false,
-	ignore_ft_on_setup = { "alpha" },
+	-- ignore_ft_on_setup = { "alpha" },
 	open_on_tab = false,
 	hijack_cursor = true,
 	hijack_unnamed_buffer_when_opening = false,
