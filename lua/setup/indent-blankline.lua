f508931aa82c136993efaf8982c58b40e8598677
Abel de la Paz <adelapazborrero@bol.com>
2023-11-22 16:27:47 +0100

    chore: add fix to current indent hightlight

--- lua/setup/indent-blankline.lua
+++ lua/setup/indent-blankline.lua
@@ -1,7 +1,17 @@
+local hooks = require("ibl.hooks")
+
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+
 require("ibl").setup({
 	scope = {
 		enabled = true,
 		show_start = false,
+		include = {
+			node_type = {
+				["*"] = { "*" },
+			},
+		},
 	},
 	indent = {
 		char = "▏",
