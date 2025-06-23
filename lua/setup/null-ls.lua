60b17dd7244aa4096f6f39ed69fad07039fb0b5a
Abel de la Paz <adelapazborrero@bol.com>
2025-06-23 16:13:59 +0200

    chore: add ruff formatting and linging for python
    Previous setup:
        - LSP: pyright
        - Linting: pyright
        - Formatting: black
    
    New setup:
        - LSP: pyright
        - Linting: ruff
        - Formatting: ruff

--- lua/setup/null-ls.lua
+++ lua/setup/null-ls.lua
@@ -1,28 +1,50 @@
-local null_ls = require("null-ls")  -- changed from "null-ls" to "none-ls"
+local null_ls = require("null-ls") -- changed from "null-ls" to "none-ls"
+local helpers = require("null-ls.helpers")
 
+-- #################################
+-- #       SUPPORTED SOURCES       #
+-- #################################
 local formatting = null_ls.builtins.formatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local sources = {
-    formatting.stylua,
-    formatting.gofmt,
-    formatting.goimports_reviser,
-    formatting.prettierd,
-    formatting.black,
+-- #################################
+-- #         CUSTOM SOURCES        #
+-- #################################
+local ruff_formatter = {
+	method = null_ls.methods.FORMATTING,
+	filetypes = { "python" },
+	generator = helpers.formatter_factory({
+		command = "ruff",
+		args = { "--fix", "--stdin-filename", "$FILENAME", "-" },
+		to_stdin = true,
+	}),
+	name = "ruff",
 }
 
+local sources = {
+	formatting.stylua,
+	formatting.gofmt,
+	formatting.goimports_reviser,
+	formatting.prettierd,
+	-- formatting.black,
+	ruff_formatter,
+}
+
+-- #################################
+-- #          LOAD SOURCES         #
+-- #################################
 null_ls.setup({
-    sources = sources,
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })  -- safer call
-                end,
-            })
-        end
-    end,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr }) -- safer call
+				end,
+			})
+		end
+	end,
 })
