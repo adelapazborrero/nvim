7deadf3c1ed88ecbf165bf25d3d2f008de2112a2
Abel de la Paz <abel45991690@gmail.com>
2025-07-20 11:57:18 +0200

    chore: add kubernetes support on lsp

--- lua/setup/lspconfig.lua
+++ lua/setup/lspconfig.lua
@@ -63,6 +63,20 @@ nvim_lsp.jsonls.setup({
 nvim_lsp.yamlls.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
+	settings = {
+		yaml = {
+			schemas = {
+				kubernetes = "k8s-*.yaml",
+				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+				["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+				["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+				["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+			},
+		},
+	},
 })
 
 -- #################################
